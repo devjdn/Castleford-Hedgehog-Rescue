617d2694b1a0bf208bc9a15a866b42c74098977d
Jayden Priestley <153106449+jhpriestley16@users.noreply.github.com>
2024-07-23 15:17:27 +0000

    applying favicon and some more content

--- chr/app/page.tsx
+++ chr/app/page.tsx
@@ -9,23 +9,36 @@ export default function Home() {
   return (
     <main>
       <section className="hero">
+        <div className="hero-overlay">
+        </div>
         <Image src={HomeHero} alt="Hero image of a hedgehog"/>
       </section>
-      <section className="donate">
-        <div className="donation-info">
-          <h2>Every donation is appreciated</h2>
-          <hr/>
-          <p>No matter the amount you donate, it is appreciated more than you will ever know. Each donation plays a vital step is stabilising the hedgehog population, and giving them another chance at life.</p>
-        </div>
-        <div className="donation-link">
-          <a href="https://www.paypal.com/donate/?hosted_button_id=D3SSPYWCDPT3W">
-            <button className="donation-btn">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-paypal" viewBox="0 0 16 16">
-                <path d="M14.06 3.713c.12-1.071-.093-1.832-.702-2.526C12.628.356 11.312 0 9.626 0H4.734a.7.7 0 0 0-.691.59L2.005 13.509a.42.42 0 0 0 .415.486h2.756l-.202 1.28a.628.628 0 0 0 .62.726H8.14c.429 0 .793-.31.862-.731l.025-.13.48-3.043.03-.164.001-.007a.35.35 0 0 1 .348-.297h.38c1.266 0 2.425-.256 3.345-.91q.57-.403.993-1.005a4.94 4.94 0 0 0 .88-2.195c.242-1.246.13-2.356-.57-3.154a2.7 2.7 0 0 0-.76-.59l-.094-.061ZM6.543 8.82a.7.7 0 0 1 .321-.079H8.3c2.82 0 5.027-1.144 5.672-4.456l.003-.016q.326.186.548.438c.546.623.679 1.535.45 2.71-.272 1.397-.866 2.307-1.663 2.874-.802.57-1.842.815-3.043.815h-.38a.87.87 0 0 0-.863.734l-.03.164-.48 3.043-.024.13-.001.004a.35.35 0 0 1-.348.296H5.595a.106.106 0 0 1-.105-.123l.208-1.32z"/>
-              </svg>
-              <p>Donate</p>
-            </button>
-          </a>
+      <section className="action">
+        <div className="section-content">
+          <div className="donate">
+            <div className="donation-info">
+              <h2>Make a donation</h2>
+              <hr/>
+              <p>No matter the amount you donate, it is appreciated more than you will ever know. Each donation plays a vital step in stabilising the hedgehog population, and giving them another chance at life.</p>
+            </div>
+            <div className="donation-link">
+              <a href="https://www.paypal.com/donate/?hosted_button_id=D3SSPYWCDPT3W">
+                <button className="donation-btn">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-paypal" viewBox="0 0 16 16">
+                    <path d="M14.06 3.713c.12-1.071-.093-1.832-.702-2.526C12.628.356 11.312 0 9.626 0H4.734a.7.7 0 0 0-.691.59L2.005 13.509a.42.42 0 0 0 .415.486h2.756l-.202 1.28a.628.628 0 0 0 .62.726H8.14c.429 0 .793-.31.862-.731l.025-.13.48-3.043.03-.164.001-.007a.35.35 0 0 1 .348-.297h.38c1.266 0 2.425-.256 3.345-.91q.57-.403.993-1.005a4.94 4.94 0 0 0 .88-2.195c.242-1.246.13-2.356-.57-3.154a2.7 2.7 0 0 0-.76-.59l-.094-.061ZM6.543 8.82a.7.7 0 0 1 .321-.079H8.3c2.82 0 5.027-1.144 5.672-4.456l.003-.016q.326.186.548.438c.546.623.679 1.535.45 2.71-.272 1.397-.866 2.307-1.663 2.874-.802.57-1.842.815-3.043.815h-.38a.87.87 0 0 0-.863.734l-.03.164-.48 3.043-.024.13-.001.004a.35.35 0 0 1-.348.296H5.595a.106.106 0 0 1-.105-.123l.208-1.32z"/>
+                  </svg>
+                  <p>Donate</p>
+                </button>
+              </a>
+            </div>
+          </div>
+          <div className="report">
+            <div className="report-info">
+              <h2>Make a report</h2>
+              <hr/>
+              <p>If you have information on a hedgehog that may need help in the Yorkshire region, please submit a report here. You can also make a report to us via our social media accounts.</p>
+            </div>
+          </div>
         </div>
       </section>
       <section className="mission">
